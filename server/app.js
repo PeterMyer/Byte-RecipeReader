7f8d141f61445ffcc81f914ae032f544fffee1f0
Peter Myer <peter.sr.myer@gmail.com>
2022-06-16 20:36:36 -0500

    changed server size restriction to 50mb

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json({limit: '50mb'}));
 
-app.use(express.urlencoded({extended: false }));
+app.use(express.urlencoded({ limit: '50mb',extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -1,8 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+// const multer  = require('multer')
+// const upload = multer()
 
-router.post('/',(req,res)=>{
-  res.send.json({status: 'OK'})
-})
+router.post("/", (req, res) => {
+  res.json({ status: "OK" });
+});
 
-module.exports = router
+
+module.exports = router;
