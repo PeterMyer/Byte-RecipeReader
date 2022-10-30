7f0149dd618aabd51c152582ee0ab519c07ae56e
Peter Myer <peter.sr.myer@gmail.com>
2022-10-30 17:43:26 -0500

    updating sequelize ssl permissions for the time being

--- server/db/db.js
+++ server/db/db.js
@@ -12,6 +12,16 @@ if(process.env.LOGGING === 'true'){
   delete config.logging
 }
 
+//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+//this is not a secure longterm solution
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
 
