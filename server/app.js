5c46033d92226a424362aca1399942ef1a84aff5
Peter Myer <peter.sr.myer@gmail.com>
2023-01-18 15:57:26 -0600

    added wildcard to app.get(/)

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
 app.use("/api", require("./api"));
 app.use("/images", express.static("server/images"))
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
