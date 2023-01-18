b82a7f545f145db2c76d2146a5994e8435cc750b
Peter Myer <peter.sr.myer@gmail.com>
2023-01-18 14:27:46 -0600

    updating route to fix heroku deploy refresh

--- server/app.js
+++ server/app.js
@@ -35,11 +35,11 @@ app.use("/images", express.static("server/images"))
 
 // app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
 
 app.get("*",(req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 })
 
 // catch 404 and forward to error handler
